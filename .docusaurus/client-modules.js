9f7109e1875269c942cb96a68fd93ab2fe1aa224
Alex K <alex.kurz@protonmail.com>
2025-05-05 16:21:53 +0200

    Deploy TinyFrameJS documentation to GitHub Pages

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/aka/Documents/AlphaQuantJS/tinyframejs-docs/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
+  require("/home/aka/Documents/AlphaQuantJS/tinyframejs-docs/node_modules/.pnpm/@docusaurus+theme-classic@3.7.0_@types+react@19.1.2_acorn@8.14.1_react-dom@19.1.0_react_f9949235f1a490b08aebf1c78a57be2d/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/aka/Documents/AlphaQuantJS/tinyframejs-docs/node_modules/.pnpm/@docusaurus+theme-classic@3.7.0_@types+react@19.1.2_acorn@8.14.1_react-dom@19.1.0_react_f9949235f1a490b08aebf1c78a57be2d/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/aka/Documents/AlphaQuantJS/tinyframejs-docs/src/css/custom.css"),
+];
